b2a043dd280775693b8636950530f7c54d7430d1
Klim <xarkoni@gmail.com>
2023-03-17 10:34:06 +0300

    first commit

--- run.js
+++ run.js
@@ -2,11 +2,10 @@
 // Запускает игру.
 const Game = require('./src/Game');
 
-// Инициализация игры с настройками.
+// Инициализация игры с настройками .
 const game = new Game({
   trackLength: 30,
 });
-
 
 // Запуск игры.
 game.play();
